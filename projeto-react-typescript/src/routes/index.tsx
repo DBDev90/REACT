1b43eeabb39af21f6eb1e78421f9d34cd08268e6
DBDev90 <bordinassidouglas@gmail.com>
2023-11-03 14:18:42 -0300

    feat: adicionando página de dashboard

---
+++ projeto-react-typescript/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+import { Dashboard } from "../pages"
+
+export const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/padina-inicial" component={Dashboard} />
+                <Route path="*" component={() => <Redirect to="/pagina-inicial" />} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
