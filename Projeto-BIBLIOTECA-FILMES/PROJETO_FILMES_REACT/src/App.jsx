3e27612d5842da38ee9e0a5a16e1f46d39f0d012
DBDev90 <bordinassidouglas@gmail.com>
2023-11-03 10:57:35 -0300

    refactor: adicionado projeto biblioteca de filmes

---
+++ Projeto-BIBLIOTECA-FILMES/PROJETO_FILMES_REACT/src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
---
+++ Projeto-BIBLIOTECA-FILMES/README.md
@@ -0,0 +1,2 @@
+# PROJETO_BIBLIOTECA_FILMES
+Projeto biblioteca de filmes utilizando React. Estudo da biblioteca React com base em video-aula do canal "Matheus Battisti - Hora de Codar" - https://bityli.com/szWmE
