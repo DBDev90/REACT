c13475f428ea4430805197e8594f7288ff5f309e
DBDev90 <bordinassidouglas@gmail.com>
2023-11-07 15:11:38 -0300

    feat: css inline

--- estilizacao-no-react/src/App.tsx
+++ estilizacao-no-react/src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 
 
 export const App = () => {
+  const [padding, setPadding] = useState(0);
+
+  useEffect(() => {
+    // setInterval(() => {
+    //   setPadding(oldPadding => {
+    //     return oldPadding + 1
+    //   })
+    // }, 2000)
+  });
+
   return (
-    <div>
+    <div style={{ backgroundColor: 'lightblue', color: "white", padding: padding }}>
       Teste
     </div>
   );
