924a7fecf45c37348530cb8e93deb2841e8612a4
DBDev90 <bordinassidouglas@gmail.com>
2023-11-07 08:35:09 -0300

    feat: passando state no contexto

--- projeto-react-typescript/src/shared/contexts/UsuarioLogado.tsx
+++ projeto-react-typescript/src/shared/contexts/UsuarioLogado.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
     nomeDoUsuario: string;
@@ -12,12 +12,20 @@ interface IUsuarioLogadoContextProps {
 export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoContextProps> = ({ children }) => {
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setNome('Douglas');
+        }, 1000);
+    });
+
     const handleLogout = useCallback(() => {
         console.log('Logout executou');
     }, []);
 
     return (
-        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: "Teste", logout: handleLogout }}>
+        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout }}>
             {children}
         </UsuarioLogadoContext.Provider>
     )
