7ed49a34f8c84242e4b8706c9ecee255fa157bdd
DBDev90 <bordinassidouglas@gmail.com>
2023-11-08 09:34:08 -0300

    feat: utilizando JSS

--- estilizacao-no-react/src/pages/dashboard/Dashboard.tsx
+++ estilizacao-no-react/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,33 @@
+import { createUseStyles } from 'react-jss'
 import './Dashboard.css'
 
+const useClasses = createUseStyles({
+    paragraph: {
+        backgroundColor: 'black',
+        color: 'white',
+        '&:hover': {
+            backgroundColor: 'red'
+        },
+        '&:active': {
+            backgroundColor: 'blue'
+        }
+    }
+});
+
 export const Dashboard = () => {
+    const classes = useClasses();
+
     return (
-        <div>
-            <p className="paragrafo">teste</p>
-        </div>
+        <>
+            <div>
+                <p>
+                    Teste
+                </p>
+            </div>
+
+            <div>
+                <p className={classes.paragraph}>Teste 2 JSS</p>
+            </div>
+        </>
     )
 }
