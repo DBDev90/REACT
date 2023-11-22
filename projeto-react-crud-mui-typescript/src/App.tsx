1dc5169e8374add65aa8c7041ba8a9afccbde632
DBDev90 <bordinassidouglas@gmail.com>
2023-11-22 14:07:52 -0300

    feat: adicionado responsividade do menu lateral

--- projeto-react-crud-mui-typescript/src/App.tsx
+++ projeto-react-crud-mui-typescript/src/App.tsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { AppThemeProvider } from "./shared/contexts";
+import { AppThemeProvider, DrawerProvider } from "./shared/contexts";
 import { MenuLateral } from "./shared/components";
 
 
@@ -9,14 +9,16 @@ import { MenuLateral } from "./shared/components";
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
+      <DrawerProvider>
+        <BrowserRouter>
 
-        <MenuLateral>
-          <AppRoutes />
-        </MenuLateral>
+          <MenuLateral>
+            <AppRoutes />
+          </MenuLateral>
 
 
-      </BrowserRouter>
+        </BrowserRouter>
+      </DrawerProvider >
     </AppThemeProvider >
   );
 }
