9694af53690f5173b1932132f688a706ea0c4d02
DBDev90 <bordinassidouglas@gmail.com>
2023-11-30 09:34:50 -0300

    feat: consulta de registro no back

--- projeto-react-crud-mui-typescript/src/pages/pessoas/DetalheDePessoas.tsx
+++ projeto-react-crud-mui-typescript/src/pages/pessoas/DetalheDePessoas.tsx
@@ -2,22 +2,57 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDeDetalhe } from "../../shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe";
+import { useEffect, useState } from "react";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { LinearProgress } from "@mui/material";
 
 export const DetalheDePessoas: React.FC = () => {
     const { id = 'nova' } = useParams<'id'>();
     const navigate = useNavigate();
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        if (id !== 'nova') {
+            setIsLoading(true);
+
+            PessoasService.getById(Number(id))
+                .then((result) => {
+                    setIsLoading(false);
+
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        navigate('/pessoas');
+                    } else {
+                        setNome(result.nomeCompleto);
+                        console.log(result);
+                    }
+                });
+        }
+    }, [id, navigate]);
+
     const handleSave = () => {
         console.log('save');
     };
 
-    const handleDelete = () => {
-        console.log('delete');
+    const handleDelete = (id: number) => {
+        if (window.confirm('Realmente deseja apagar o registro?')) {
+            PessoasService.deleteById(id)
+                .then(result => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        alert('Registro apagado com sucesso!');
+                        navigate('/pessoas');
+                    }
+                });
+        }
     };
 
     return (
         <LayoutBaseDePagina
-            titulo="Detalhe de pessoa"
+            titulo={id === 'nova' ? 'Nova pessoa' : nome}
             barraDeFerramentas={
                 <FerramentasDeDetalhe
                     textoBotaoNovo="Nova"
@@ -27,13 +62,15 @@ export const DetalheDePessoas: React.FC = () => {
 
                     aoCLicarEmSalvar={handleSave}
                     aoClicarEmSalvarFechar={handleSave}
-                    aoClicarEmApagar={handleDelete}
+                    aoClicarEmApagar={() => handleDelete(Number(id))}
                     aoClicarEmVoltar={() => navigate('/pessoas')}
                     aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
                 />
             }
         >
-
+            {isLoading && (
+                <LinearProgress variant="indeterminate" />
+            )}
         </LayoutBaseDePagina>
     )
 }
