79e77cdcdedd85db1a7878f2a968a1bdaa6dcfad
DBDev90 <bordinassidouglas@gmail.com>
2023-12-01 11:00:19 -0300

    feat: componetizando logica

--- projeto-react-crud-mui-typescript/src/pages/pessoas/DetalheDePessoas.tsx
+++ projeto-react-crud-mui-typescript/src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Form } from "@unform/web";
-import { FormHandles } from "@unform/core";
 
-import { VTextField } from "../../shared/forms";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { VTextField, VForm, useVForm } from "../../shared/forms";
+import { Box, Grid, LinearProgress, Paper, Typography } from "@mui/material";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { FerramentasDeDetalhe } from "../../shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe";
-import { Box, Grid, LinearProgress, Paper, Typography } from "@mui/material";
 
 interface IFormData {
     email: string;
@@ -19,7 +17,7 @@ export const DetalheDePessoas: React.FC = () => {
     const { id = 'nova' } = useParams<'id'>();
     const navigate = useNavigate();
 
-    const formRef = useRef<FormHandles>(null);
+    const { formRef, save, isSaveAndNew, isSaveAndClose } = useVForm();
 
     const [isLoading, setIsLoading] = useState(false);
     const [nome, setNome] = useState('');
@@ -40,6 +38,12 @@ export const DetalheDePessoas: React.FC = () => {
                         formRef.current?.setData(result);
                     }
                 });
+        } else {
+            formRef.current?.setData({
+                email: '',
+                codCidade: '',
+                nomeCompleto: '',
+            })
         }
     }, [id, navigate]);
 
@@ -55,7 +59,11 @@ export const DetalheDePessoas: React.FC = () => {
                     if (result instanceof Error) {
                         alert(result.message);
                     } else {
-                        navigate(`/pessoas/detalhe/${result}`)
+                        if (isSaveAndClose()) {
+                            navigate('/pessoas');
+                        } else {
+                            navigate(`/pessoas/detalhe/${result}`);
+                        }
                     }
                 });
         } else {
@@ -66,6 +74,10 @@ export const DetalheDePessoas: React.FC = () => {
 
                     if (result instanceof Error) {
                         alert(result.message);
+                    } else {
+                        if (isSaveAndClose()) {
+                            navigate('/pessoas');
+                        }
                     }
                 });
         }
@@ -95,15 +107,15 @@ export const DetalheDePessoas: React.FC = () => {
                     mostrarBotaoNovo={id !== 'nova'}
                     mostrarBotaoApagar={id !== 'nova'}
 
+                    aoCLicarEmSalvar={save}
+                    aoClicarEmSalvarFechar={isSaveAndClose}
                     aoClicarEmVoltar={() => navigate('/pessoas')}
                     aoClicarEmApagar={() => handleDelete(Number(id))}
-                    aoCLicarEmSalvar={() => formRef.current?.submitForm()}
                     aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
-                    aoClicarEmSalvarFechar={() => formRef.current?.submitForm()}
                 />
             }
         >
-            <Form ref={formRef} onSubmit={handleSave}>
+            <VForm ref={formRef} onSubmit={handleSave}>
                 <Box margin={1} display="flex" flexDirection="column" component={Paper} variant="outlined">
 
                     <Grid container direction="column" padding={2}>
@@ -146,11 +158,7 @@ export const DetalheDePessoas: React.FC = () => {
                         </Grid>
                     </Grid>
                 </Box>
-            </Form>
-
-            {/* {isLoading && (
-                <LinearProgress variant="indeterminate" />
-            )} */}
+            </VForm>
         </LayoutBaseDePagina>
     )
 }
